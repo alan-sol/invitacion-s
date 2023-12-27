da6257e6a039d321db9680821fd7a5f4b68754f3
alan-sol <143287034+alan-sol@users.noreply.github.com>
2023-12-26 23:16:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const countdownContainer = document.querySelector('.contador');
+const daysSpan = countdownContainer.querySelector('.days');
+const hoursSpan = countdownContainer.querySelector('.hours');
+const minutesSpan = countdownContainer.querySelector('.minutes');
+const secondsSpan = countdownContainer.querySelector('.seconds');
+
+function updateCountdown() {
+  const targetDate = new Date('2024-01-27T19:59:59'); // Cambia a la fecha objetivo
+  const currentDate = new Date();
+  const timeDifference = targetDate - currentDate;
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  daysSpan.textContent = days;
+  hoursSpan.textContent = hours;
+  minutesSpan.textContent = minutes;
+  secondsSpan.textContent = seconds;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
+
+// S L I D E R
+document.addEventListener("DOMContentLoaded", function (){
+  const images = document.querySelectorAll(".image");
+  let currentIndex = 0;
+
+  function showImage (index) {
+    images.forEach((image, i) => {
+      image.classList.toggle("active", i === index);
+    });
+  }
+
+  function nextImage (){
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
+
+  setInterval(nextImage, 3000);
+  showImage(currentIndex);
+})
+
+
+//M U S I C A
+let sound = new Audio('./audio/Ed-Sheeran-Perfect.mp3');
+
+playBtn.addEventListener('click', () =>{
+  sound.play();
+})
+
+playBtn.addEventListener('touchstart', () =>{
+  sound.play();
+})
+
+pauseBtn.addEventListener('touchstart', () =>{
+  sound.pause();
+})
+
+pauseBtn.addEventListener('click', () =>{
+  sound.pause();
+})
+
+
+//L O A D E R
+window.addEventListener("load", function () {
+  var onloadHTML = document.getElementById('onload');
+  var bodyHTML = document.getElementById('bodyAr');
+    
+  onloadHTML.style.display = 'none';
+  bodyHTML.classList.remove('hidden');
+});
